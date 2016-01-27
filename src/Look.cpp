bec4833892ed46418053f0b714d5f64358445cee
cxsquared <cxsquared@gmail.com>
2016-01-26 20:08:19 -0500

    made it so you can look at items with the look comand.

--- src/Look.cpp
+++ src/Look.cpp
@@ -1,9 +1,29 @@
 #include "Look.h"
 #include "GameState.h"
+#include "Item.h"
 
 string Look::parseNoun(vector<string> options, ScreenState * state)
 {
 	GameState* game = dynamic_cast<GameState*>(state);
+	
+	if (options.size() > 0) {
+		if (options[0] == "at" && options.size() > 1) {
+			for (auto item : game->level->player->currentRoom->items) {
+				if (item->getName() == options[1]) {
+					return item->Interact(*game->level);
+				}
+			}
+		}
+		else {
+			for (auto item : game->level->player->currentRoom->items) {
+				if (item->getName() == options[0]) {
+					return item->Interact(*game->level);
+				}
+			}
+		}
+
+		return "There isn't anything like that in the room.";
+	}
 
 	return game->level->player->currentRoom->getDescritption();
 }
